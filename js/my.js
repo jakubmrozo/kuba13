ce505c2a0ddf38f839be6629405b87b3478094b1
Your Name <you@example.com>
2019-12-05 18:56:53 +0100

    lesson13

---
+++ js/my.js
@@ -0,0 +1,25 @@
+let result =10;
+console.log("tcl: result", result);
+
+
+if (100 - 10 > 80) {
+    alert ("correct")
+    } else {
+    alert ("not correct")
+    }
+
+
+    document.getElementById("result").textContent = result;
+
+let counter = 2;
+counter = counter + 3
+console.log(counter)
+
+const left = '10';
+const right = 10;
+console.log(left === right)
+
+function add(2,6)
+return 2+6;
+
+
